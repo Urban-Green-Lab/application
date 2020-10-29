4fd090311a74ef21d9faf1ba927305dbe8f7cf11
Trinity Terry <trinitycterry@gmail.com>
2020-10-28 23:23:11 -0500

    Added Tutorial template

--- api/djangorestapp/admin.py
+++ api/djangorestapp/admin.py
@@ -1,16 +1,17 @@
 from . import models
 from django.contrib.admin import AdminSite
 from django.template.response import TemplateResponse
-from django.http import HttpResponse
 from django.urls import path
 
 
 class MyAdminSite(AdminSite):
+    index_template = "tutorial.html"
+    index_title = None
 
     def get_urls(self):
         urls = super().get_urls()
         urls += [
-            path('my_view/', self.admin_view(self.my_view))
+            path('help/', self.admin_view(self.my_view))
         ]
         return urls
 
@@ -18,15 +19,14 @@ class MyAdminSite(AdminSite):
         print(request.user, "*********************")
         context = dict(
             self.each_context(request),
-            title=('Twsdfsrsr'),
             app_path=None,
             username=request.user.get_username(),
         )
-        return TemplateResponse(request, "sometemplate.html", context)
+        return TemplateResponse(request, "tutorial.html", context)
 
 
 admin_site = MyAdminSite(name='myadmin')
-admin_site.site_url = None
+# admin_site.index_template = 'sometemplate.html'
 # Register your models here.
 
 admin_site.register(models.EventQuiz)
