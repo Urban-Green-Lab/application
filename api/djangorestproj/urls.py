b21cfe2bce510433dc3a6525168f5de1f4d4783b
Trinity Terry <trinitycterry@gmail.com>
2020-10-30 15:19:47 -0500

    removed extra

--- api/djangorestproj/urls.py
+++ api/djangorestproj/urls.py
@@ -13,13 +13,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
 from django.urls import path, include
 from djangorestapp.admin import admin_site
 from djangorestapp import views
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-from djangorestproj import settings
-from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin_site.urls),
