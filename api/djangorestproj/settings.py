e1d786fa4af64d75a350e9746e17fdf0f88fa89e
Trinity Terry <trinitycterry@gmail.com>
2020-10-30 08:19:40 -0500

    setup for heroku deployment

--- api/djangorestproj/settings.py
+++ api/djangorestproj/settings.py
@@ -14,6 +14,7 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 # Pull in IS_HEROKU to see if in production or not
 is_prod = os.getenv('IS_HEROKU', False)
@@ -143,6 +144,8 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+
 
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
