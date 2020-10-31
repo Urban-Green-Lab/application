423a8c85794722fefb0a3828333d73428450c073
Dr. Teresa Vasquez <dr.teresavasquez@gmail.com>
2020-10-31 18:34:48 -0500

    updated scoring model

--- browser/src/data/getActiveEvent.js
+++ browser/src/data/getActiveEvent.js
@@ -11,7 +11,7 @@ const getActiveEvent = () => new Promise((resolve) => {
   axios.get('https://sustaingame-admin.herokuapp.com/active_event_info/')
     .then((response) => resolve(response.data))
     .catch((error) => {
-      if (error.response) {
+      if (error) {
         resolve('NOPE');
       }
     });
