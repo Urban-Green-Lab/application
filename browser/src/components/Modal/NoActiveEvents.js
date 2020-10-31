cf7ff1a12e6289a318c73ba2f8d4d94df1ceb13c
Dr. Teresa Vasquez <dr.teresavasquez@gmail.com>
2020-10-31 18:39:24 -0500

    updated scoring model

--- browser/src/components/Modal/NoActiveEvents.js
+++ browser/src/components/Modal/NoActiveEvents.js
@@ -19,13 +19,13 @@ const NoActiveEvents = (props) => {
 
   return (
     <div className="modal-container">
-      <Button className="btn-dark" color="info" onClick={toggle}>{buttonLabel}</Button>
+      <Button className="btn-dark mb-4" color="info" onClick={toggle}>{buttonLabel}</Button>
       <Modal isOpen={modal} toggle={toggle} className={className}>
         <ModalHeader toggle={toggle} close={closeBtn}></ModalHeader>
-        <ModalBody>
+        <ModalBody className="mx-4">
           <img src={sustaingame} alt="sustain game" className="sustaingame-img"/>
           <h1>No Active Events</h1>
-          <p>Currently there are not any active events happening. Check out "How to Play" to be prepared for when the next challenge is available</p>
+          <p className="container">Currently there are not any active events happening. Check out "How to Play" to be prepared for when the next challenge is available</p>
 
           <p>In the meantime, checkout our <a href="https://urbangreenlab.org/">website</a> for more details about what we do and who we are.</p>
         </ModalBody>
